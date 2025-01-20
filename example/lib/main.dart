201abdf8d3dc07a679af9ff9ec2b3f3552325510
Rollfire <71181173+Rollfreeze@users.noreply.github.com>
2025-01-20 23:31:28 +0300

    Add default size

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,8 +8,6 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  static const _boxSize = 200.0;
-  static const _indent = 20.0;
   static const _bgColor = Color.fromRGBO(253, 247, 254, 1);
 
   @override
@@ -19,119 +17,104 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: _bgColor,
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(_indent),
-            child: SingleChildScrollView(
-              child: Column(
-                spacing: 20,
-                children: [
-                  const ProgressCircle(
-                    boxSize: _boxSize,
-                    completed: 2,
-                    total: 4,
-                    completedPercent: 16,
-                    style: ProgressCircleStyle(
-                      innerColor: _bgColor,
-                    ),
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 20,
+              children: [
+                const ProgressCircle(
+                  completed: 2,
+                  total: 4,
+                  completedPercent: 16,
+                  style: ProgressCircleStyle(
+                    innerColor: _bgColor,
                   ),
-                  const ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 1,
-                    total: 4,
-                    style: ProgressCircleStyle(
-                      innerColor: _bgColor,
-                    ),
+                ),
+                const ProgressCircle.fromValues(
+                  completed: 1,
+                  total: 4,
+                  style: ProgressCircleStyle(
+                    innerColor: _bgColor,
                   ),
-                  ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 2,
-                    total: 4,
-                    style: ProgressCircleStyle(
-                      headIcon: Icons.star,
-                      tailIcon: Icons.apple,
-                      progressArcColor: Colors.orange,
-                      innerColor: Colors.orange.shade100,
-                      centerMessage: '50 %',
-                    ),
+                ),
+                ProgressCircle.fromValues(
+                  completed: 2,
+                  total: 4,
+                  style: ProgressCircleStyle(
+                    headIcon: Icons.star,
+                    tailIcon: Icons.apple,
+                    progressArcColor: Colors.orange,
+                    innerColor: Colors.orange.shade100,
+                    centerMessage: '50 %',
                   ),
-                  const ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 5,
-                    total: 6,
-                    style: ProgressCircleStyle(
-                      centerMessage: '5 / 6',
-                      innerColor: _bgColor,
-                      progressArcColor: Colors.pinkAccent,
-                      headIcon: Icons.star,
-                      tailIcon: Icons.apple,
-                    ),
+                ),
+                const ProgressCircle.fromValues(
+                  completed: 5,
+                  total: 6,
+                  style: ProgressCircleStyle(
+                    centerMessage: '5 / 6',
+                    innerColor: _bgColor,
+                    progressArcColor: Colors.pinkAccent,
+                    headIcon: Icons.star,
+                    tailIcon: Icons.apple,
                   ),
-                  ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 4,
-                    total: 4,
-                    style: ProgressCircleStyle(
-                      innerColor: Colors.green.shade600,
-                      progressArcColor: Colors.green.shade900,
-                    ),
+                ),
+                ProgressCircle.fromValues(
+                  completed: 4,
+                  total: 4,
+                  style: ProgressCircleStyle(
+                    innerColor: Colors.green.shade600,
+                    progressArcColor: Colors.green.shade900,
                   ),
-                  ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 4,
-                    total: 4,
-                    style: ProgressCircleStyle(
-                      headIcon: Icons.star,
-                      tailIcon: Icons.apple,
-                      innerColor: Colors.orange.shade100,
-                      progressArcColor: Colors.orange.shade600,
-                    ),
+                ),
+                ProgressCircle.fromValues(
+                  completed: 4,
+                  total: 4,
+                  style: ProgressCircleStyle(
+                    headIcon: Icons.star,
+                    tailIcon: Icons.apple,
+                    innerColor: Colors.orange.shade100,
+                    progressArcColor: Colors.orange.shade600,
                   ),
-                  ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 4,
-                    total: 4,
-                    style: ProgressCircleStyle(
-                      innerColor: Colors.red.shade900,
-                      progressArcColor: Colors.red,
-                      headIcon: Icons.star,
-                      tailIcon: Icons.apple,
-                    ),
+                ),
+                ProgressCircle.fromValues(
+                  completed: 4,
+                  total: 4,
+                  style: ProgressCircleStyle(
+                    innerColor: Colors.red.shade900,
+                    progressArcColor: Colors.red,
+                    headIcon: Icons.star,
+                    tailIcon: Icons.apple,
                   ),
-                  const ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 6,
-                    total: 8,
-                    style: ProgressCircleStyle(
-                      arcWidth: 15,
-                    ),
+                ),
+                const ProgressCircle.fromValues(
+                  completed: 6,
+                  total: 8,
+                  style: ProgressCircleStyle(
+                    arcWidth: 15,
                   ),
-                  const ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 2,
-                    total: 4,
-                    style: ProgressCircleStyle(
-                      arcWidth: 30,
-                    ),
+                ),
+                const ProgressCircle.fromValues(
+                  completed: 2,
+                  total: 4,
+                  style: ProgressCircleStyle(
+                    arcWidth: 30,
                   ),
-                  const ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 3,
-                    total: 8,
-                    style: ProgressCircleStyle(
-                      arcWidth: 60,
-                    ),
+                ),
+                const ProgressCircle.fromValues(
+                  completed: 3,
+                  total: 8,
+                  style: ProgressCircleStyle(
+                    arcWidth: 60,
                   ),
-                  const ProgressCircle.fromPercent(
-                    boxSize: _boxSize,
-                    completedPercent: 0,
-                  ),
-                  const ProgressCircle.fromValues(
-                    boxSize: _boxSize,
-                    completed: 5,
-                    total: 10,
-                  ),
-                ],
-              ),
+                ),
+                const ProgressCircle.fromPercent(
+                  completedPercent: 0,
+                ),
+                const ProgressCircle.fromValues(
+                  completed: 5,
+                  total: 10,
+                ),
+              ],
             ),
           ),
         ),
--- lib/src/progress_circle.dart
+++ lib/src/progress_circle.dart
@@ -7,33 +7,30 @@ class ProgressCircle extends StatelessWidget {
   /// Creates a default ProgressCircle.
   const ProgressCircle({
     super.key,
-    required this.boxSize,
     this.total = 0,
     this.completed = 0,
     this.completedPercent,
+    this.size = const Size.fromRadius(100),
     this.style = const ProgressCircleStyle(),
   });
 
   /// ProgressCircle based on `total` and `completed` values.
   const ProgressCircle.fromValues({
     super.key,
-    required this.boxSize,
     required this.total,
     required this.completed,
+    this.size = const Size.fromRadius(100),
     this.style = const ProgressCircleStyle(),
   }) : completedPercent = null;
 
   /// ProgressCircle based on `completedPercent` value.
   const ProgressCircle.fromPercent({
     super.key,
-    required this.boxSize,
     required this.completedPercent,
+    this.size = const Size.fromRadius(100),
     this.style = const ProgressCircleStyle(),
   })  : total = 0,
         completed = 0;
-
-  /// A size of the widget.
-  final double boxSize;
 
   /// Total value.
   final double total;
@@ -44,6 +41,9 @@ class ProgressCircle extends StatelessWidget {
   /// Complete percent e.g. 34.7%.
   final double? completedPercent;
 
+  /// Size of the widget.
+  final Size size;
+
   /// Style properties for the widget.
   final ProgressCircleStyle style;
 
@@ -51,7 +51,7 @@ class ProgressCircle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: CustomPaint(
-        size: Size.square(boxSize),
+        size: size,
         painter: ProgressCirclePainter(
           total: total,
           completed: completed,
