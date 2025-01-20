ca871298b76cc418af2411b54966f0df13e9de98
Rollfire <71181173+Rollfreeze@users.noreply.github.com>
2025-01-20 23:18:50 +0300

    Add ProgressCircleStyle

--- lib/progress_circle.dart
+++ lib/progress_circle.dart
@@ -1,3 +1,4 @@
 library progress_circle;
 
 export 'src/progress_circle.dart';
+export 'src/progress_circle_style.dart';
---
+++ lib/src/progress_circle_style.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+/// Style properties of the widget.
+class ProgressCircleStyle {
+  /// Creates a default style.
+  const ProgressCircleStyle({
+    this.progressArcColor = Colors.blue,
+    this.arcWidth = 35,
+    this.isRoundedHead = true,
+    this.headIcon,
+    this.headIconSize = 15.0,
+    this.headIconColor = Colors.white,
+    this.isRoundedTail = true,
+    this.tailIcon,
+    this.tailIconSize = 15.0,
+    this.tailIconColor = Colors.white,
+    this.centerMessage,
+    this.centerMessageStyle,
+    this.innerColor = Colors.white,
+    this.arcColor = const Color(0XFFEFEFF4),
+  });
+
+  /// A color of the progress curve.
+  final Color progressArcColor;
+
+  /// A Circle's arc width.
+  final double arcWidth;
+
+  /// Should the head of the curve line be rounded.
+  final bool isRoundedHead;
+
+  /// The head's icon.
+  final IconData? headIcon;
+
+  /// The head icon size.
+  final double headIconSize;
+
+  /// The head's icon color.
+  final Color headIconColor;
+
+  /// Should the tail of the curve line be rounded.
+  final bool isRoundedTail;
+
+  /// The head's icon.
+  final IconData? tailIcon;
+
+  /// The head icon size.
+  final double tailIconSize;
+
+  /// The tails's icon color.
+  final Color tailIconColor;
+
+  /// An optional message in the center.
+  final String? centerMessage;
+
+  /// A text style for the center message.
+  final TextStyle? centerMessageStyle;
+
+  /// A color behind the circle.
+  final Color innerColor;
+
+  /// A color of the circle's arc.
+  final Color arcColor;
+}
